954ad0b106fe1e19f7ec68fa562d20ec9718b6be
Dave K <davecontrollive@gmail.com>
2022-10-02 07:36:10 -0700

    Movement

---
+++ Cloud Drift/Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] float animationSpeed = 1f;
+
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset += new Vector2(animationSpeed * Time.deltaTime, 0);
+    }
+}
---
+++ Cloud Drift/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+
+    Vector2 moveDirection;
+
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float moveAmount = moveSpeed * Time.deltaTime;
+
+        moveDirection.x = Input.GetAxisRaw("Horizontal") * moveAmount;
+        moveDirection.y = Input.GetAxisRaw("Vertical") * moveAmount;
+
+        transform.Translate(moveDirection.x, moveDirection.y, 0);
+    }
+}
