a3e2e1c4a886a81540b3464ee3546020a4f8325a
Dave K <davecontrollive@gmail.com>
2022-10-12 10:45:20 -0700

    Sinwave Enemy Behavior
    Added a sinwave for the Carrot enemy behavior that can be adjusted by move speed, amplitude, and frequency.

---
+++ Cloud Drift/Assets/Scripts/CarrotMover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrotMover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float amplitude = 2f;
+    [SerializeField] float frequency = 0.5f;
+
+    [SerializeField] bool inverted = false;
+
+    float sinCenterY;
+
+    void Start()
+    {
+        sinCenterY = transform.position.y;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        MoveRightToLeft();
+        SinMovement();
+    }
+
+    void MoveRightToLeft()
+    {
+        Vector2 pos = transform.position;
+        pos.x -= moveSpeed * Time.deltaTime;
+        transform.position = pos;
+    }
+
+    void SinMovement()
+    {
+        Vector2 pos = transform.position;
+        float sin = Mathf.Sin(pos.x * frequency) * amplitude;
+        if (inverted)
+        {
+            sin *= -1;
+        }
+        pos.y = sinCenterY + sin;
+
+        transform.position = pos;
+    }
+}
