9b6e3487d9b4cfa5ae831618a7b5b6f632bf2f1b
Dave K <davecontrollive@gmail.com>
2022-10-14 00:01:28 -0700

    Player Death
    The player now takes damage from hitting enemies.
    The player now flashes when hit.
    The player now explodes when dies.
    All children animations are turned off prior to death.
    SFX play for all of these.

--- Cloud Drift/Assets/Scripts/PlayerHealth.cs
+++ Cloud Drift/Assets/Scripts/PlayerHealth.cs
@@ -7,12 +7,33 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField] int maxHealth = 50;
     [SerializeField] int currentHealth = 50;
 
+    Animator shipAnimator;
+    UpgradeSwitcher upgradeSwitcher;
+    AudioPlayer audioPlayer;
+
+    bool isDead = false;
+
+    void Awake()
+    {
+        audioPlayer = FindObjectOfType<AudioPlayer>();
+    }
+
+    void Start()
+    {
+        shipAnimator = GetComponent<Animator>();
+        upgradeSwitcher = GetComponent<UpgradeSwitcher>();
+    }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        if (currentHealth <= 0)
+        if (currentHealth <= 0 && !isDead)
         {
-            Destroy(gameObject);
+            StartCoroutine(Die());
+        }
+        else
+        {
+            StartCoroutine(PlayerDamaged());
         }
     }
 
@@ -23,5 +44,35 @@ public class PlayerHealth : MonoBehaviour
         {
             currentHealth = maxHealth;
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            TakeDamage(other.GetComponent<DamageDealer>().GetDamage());
+        }
+    }
+
+    IEnumerator PlayerDamaged()
+    {
+        audioPlayer.PlayDamageClip();
+        GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 1f);
+        yield return new WaitForSeconds(0.05f);
+        GetComponent<SpriteRenderer>().material.SetFloat("_FlashAmount", 0f);
+    }
+
+    IEnumerator Die()
+    {
+        isDead = true;
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        audioPlayer.PlayPlayerDeathClip();
+        shipAnimator.SetTrigger("PlayerDeathTrigger");
+        yield return new WaitForSeconds(0.8f);
+        Destroy(gameObject);
     }
 }
