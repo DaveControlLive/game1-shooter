2ce6fa0efebdf2e5340587c67517a794efa60512
Dave K <davecontrollive@gmail.com>
2022-10-26 17:33:15 -0700

    HUD transparency
    - Added transparency to the HUD so if you fly under it, it will go slightly transparent so it's not in the way.

--- Cloud Drift/Assets/Scripts/UI/UIDisplay.cs
+++ Cloud Drift/Assets/Scripts/UI/UIDisplay.cs
@@ -4,76 +4,103 @@ using UnityEngine;
 using UnityEngine.UI;
 using Shooter.Core;
 
-public class UIDisplay : MonoBehaviour
+namespace Shooter.UI
 {
-    [Header("Health")]
-    [SerializeField] Slider healthSlider;
-    PlayerHealth playerHealth;
-
-    [Header("PowerUp")]
-    [SerializeField] Slider powerSlider;
-    [SerializeField] int maxPower = 2;
-    int currentPower;
-
-    [Header("SpeedUp")]
-    [SerializeField] Slider speedSlider;
-    [SerializeField] int maxSpeed = 2;
-    int currentSpeed;
-
-    [Header("Lives")]
-    [SerializeField] Transform[] livesDisplay;
-    int currentLives = 3;
-
-    UpgradeSwitcher upgradeSwitcher;
-    GameSession gameSession;
-
-    void Awake()
+    public class UIDisplay : MonoBehaviour
     {
-        playerHealth = FindObjectOfType<PlayerHealth>();
-        upgradeSwitcher = FindObjectOfType<UpgradeSwitcher>();
-        gameSession = FindObjectOfType<GameSession>();
-    }
+        Transform player;
 
-    void Start()
-    {
-        healthSlider.maxValue = playerHealth.GetMaxHealth();
-        powerSlider.maxValue = maxPower;
-        speedSlider.maxValue = maxSpeed;
-        UpdateLives();
-    }
+        [Header("HUD Hide Alpha")]
+        [SerializeField] float transparencyAmount = 0.5f;
 
-    void Update()
-    {
-        UpdateHealth();
-        UpdateUpgrades();
-    }
+        [Header("Health")]
+        [SerializeField] Slider healthSlider;
+        PlayerHealth playerHealth;
 
-    void UpdateHealth()
-    {
-        healthSlider.value = playerHealth.GetCurrentHealth();
-    }
+        [Header("PowerUp")]
+        [SerializeField] Slider powerSlider;
+        [SerializeField] int maxPower = 2;
+        int currentPower;
 
-    void UpdateUpgrades()
-    {
-        powerSlider.value = upgradeSwitcher.GetCurrentWeaponUpgrade();
-        speedSlider.value = upgradeSwitcher.GetCurrentSpeedUpgrade();
-    }
+        [Header("SpeedUp")]
+        [SerializeField] Slider speedSlider;
+        [SerializeField] int maxSpeed = 2;
+        int currentSpeed;
 
-    void UpdateLives()
-    {
-        int newLives = gameSession.GetInstance().GetCurrentLives();
-        if (currentLives != newLives)
+        [Header("Lives")]
+        [SerializeField] Transform[] livesDisplay;
+        int currentLives = 3;
+
+        UpgradeSwitcher upgradeSwitcher;
+        GameSession gameSession;
+
+        bool transparentUI = false;
+
+        void Awake()
         {
-            livesDisplay[currentLives - 1].GetComponent<Image>().enabled = false;
-            currentLives = newLives;
-            livesDisplay[currentLives - 1].GetComponent<Image>().enabled = true;
+            playerHealth = FindObjectOfType<PlayerHealth>();
+            player = playerHealth.GetComponent<Transform>();
+            upgradeSwitcher = FindObjectOfType<UpgradeSwitcher>();
+            gameSession = FindObjectOfType<GameSession>();
+        }
+
+        void Start()
+        {
+            healthSlider.maxValue = playerHealth.GetMaxHealth();
+            powerSlider.maxValue = maxPower;
+            speedSlider.maxValue = maxSpeed;
+            UpdateLives();
+        }
+
+        void Update()
+        {
+            UpdateTransparency();
+            UpdateHealth();
+            UpdateUpgrades();
+        }
+
+        void UpdateTransparency()
+        {
+            if (!transparentUI && player.position.x < -3.83 && player.position.y < -2.54)
+            {
+                GetComponent<CanvasGroup>().alpha = transparencyAmount;
+                transparentUI = !transparentUI;
+            }
+            if (transparentUI && (player.position.x > -3.83 || player.position.y > -2.54))
+            {
+                GetComponent<CanvasGroup>().alpha = 1f;
+                transparentUI = !transparentUI;
+            }
+        }
+
+        void UpdateHealth()
+        {
+            healthSlider.value = playerHealth.GetCurrentHealth();
+        }
+
+        void UpdateUpgrades()
+        {
+            powerSlider.value = upgradeSwitcher.GetCurrentWeaponUpgrade();
+            speedSlider.value = upgradeSwitcher.GetCurrentSpeedUpgrade();
+        }
+
+        void UpdateLives()
+        {
+            int newLives = gameSession.GetInstance().GetCurrentLives();
+            if (currentLives != newLives)
+            {
+                livesDisplay[currentLives - 1].GetComponent<Image>().enabled = false;
+                currentLives = newLives;
+                livesDisplay[currentLives - 1].GetComponent<Image>().enabled = true;
+            }
+        }
+
+        public void ResetValues()
+        {
+            playerHealth = FindObjectOfType<PlayerHealth>();
+            upgradeSwitcher = FindObjectOfType<UpgradeSwitcher>();
+            print(playerHealth.GetCurrentHealth());
         }
     }
 
-    public void ResetValues()
-    {
-        playerHealth = FindObjectOfType<PlayerHealth>();
-        upgradeSwitcher = FindObjectOfType<UpgradeSwitcher>();
-        print(playerHealth.GetCurrentHealth());
-    }
 }
