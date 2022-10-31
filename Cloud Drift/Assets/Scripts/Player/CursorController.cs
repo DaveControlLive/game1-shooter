3e8f132fa0b8d9c1565c48162ea3868b75535884
Dave K <davecontrollive@gmail.com>
2022-10-31 11:42:50 -0700

    Title Screen Added
    - Title screen has been created using Unity Canvas
    - Needs SFX for clicking up/down/start, but otherwise it's good to go.
    - Need to create Death scene

---
+++ Cloud Drift/Assets/Scripts/Player/CursorController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Shooter.Core;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] Vector3 positionUp = new Vector3(-189, -249, 0);
+    [SerializeField] Vector3 positionDown = new Vector3(-189, -312, 0);
+
+    bool startGame = true;
+    GameSession gameSession;
+
+    void Awake()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+    }
+
+    void Start()
+    {
+        GetComponent<RectTransform>().localPosition = positionUp;
+    }
+
+    void Update()
+    {
+        MoveCursor();
+    }
+
+    void MoveCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            GetComponent<RectTransform>().localPosition = positionDown;
+            startGame = false;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<RectTransform>().localPosition = positionUp;
+            startGame = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (startGame)
+            {
+                gameSession.AccessNextLevel();
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
