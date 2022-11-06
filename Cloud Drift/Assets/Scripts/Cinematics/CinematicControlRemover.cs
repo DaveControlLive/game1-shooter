618e86bd66813b3f3482eec34fa0e363815fdcc4
Dave K <davecontrollive@gmail.com>
2022-11-06 12:11:07 -0800

    Title Animation Added
    - Title Animation has been added!
    - Player Control is disabled when animation is playing, and re-enabled when it stops playing.

---
+++ Cloud Drift/Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinematicControlRemover : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Cursor");
+        }
+
+        GetComponent<PlayableDirector>().played += DisableControl;
+        GetComponent<PlayableDirector>().stopped += EnableControl;
+    }
+
+    void DisableControl(PlayableDirector pm)
+    {
+        if (player.tag == "Player")
+        {
+            player.GetComponent<PlayerController>().enabled = false;
+        }
+        if (player.tag == "Cursor")
+        {
+            player.GetComponent<CursorController>().enabled = false;
+        }
+    }
+
+    void EnableControl(PlayableDirector pm)
+    {
+        if(player.tag == "Player")
+        {
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+        if(player.tag == "Cursor")
+        {
+            player.GetComponent<CursorController>().enabled = true;
+        }
+    }
+}
