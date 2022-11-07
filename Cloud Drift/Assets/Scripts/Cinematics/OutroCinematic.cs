f4e4e0a6a29440927ded7d0286eb8f412e363693
Dave K <davecontrollive@gmail.com>
2022-11-06 18:15:30 -0800

    Outro Cutscene
    - Don't love the way I implemented it, but it works.
    - I call a script that lerps the player to the center of the screen.
    - Once the player is there, the cutscene is basically triggered, which has the player in the correct place for flying offscreen.

---
+++ Cloud Drift/Assets/Scripts/Cinematics/OutroCinematic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class OutroCinematic : MonoBehaviour
+{
+    [SerializeField] float playerMoveSpeed = 0.5f;
+    [SerializeField] float timeUntilCutscene = 3f;
+
+    Vector3 startingPosition;
+    Vector3 endingPosition = new Vector3(0, 0, 0);
+    GameObject player;
+    float travelTimer = 0f;
+    float cutsceneTimer = 0f;
+
+    bool cutscenePlayed = false;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        player.GetComponent<PlayerController>().enabled = false;
+        startingPosition = player.transform.localPosition;
+        StartCoroutine(MoveToCenter());
+    }
+
+    void Update()
+    {
+        if (!cutscenePlayed && cutsceneTimer >= timeUntilCutscene)
+        {
+            cutscenePlayed = true;
+            PlayCutscene();
+        }
+
+        UpdateTimers();
+    }
+
+    IEnumerator MoveToCenter()
+    {
+        float travelPercent = 0f;
+        while (travelPercent < 1f && !cutscenePlayed)
+        {
+            Vector3 startPosition = player.transform.localPosition;
+            Vector3 endPosition = new Vector3(0,0,0);
+            player.transform.localPosition = Vector3.Lerp(startPosition, endPosition, travelPercent);
+            travelPercent += Time.deltaTime * playerMoveSpeed;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    void PlayCutscene()
+    {
+        GetComponent<PlayableDirector>().Play();
+    }
+
+    void UpdateTimers()
+    {
+        travelTimer += Time.deltaTime * playerMoveSpeed;
+        cutsceneTimer += Time.deltaTime;
+    }
+}
---
+++ Cloud Drift/Assets/Scripts/Cinematics/OutroPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutroPlayer : MonoBehaviour
+{
+    OutroCinematic outroCinematic;
+
+    void Awake()
+    {
+        outroCinematic = FindObjectOfType<OutroCinematic>();
+    }
+
+    void Start()
+    {
+        outroCinematic.enabled = true;
+        Destroy(gameObject);
+    }
+}
